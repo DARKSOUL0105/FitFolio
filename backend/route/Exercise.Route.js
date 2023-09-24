03254fe88fa274798a1f5e4dfde0a53a7e246322
Jarvis <kamaleshselvam75@gmail.com>
2023-09-24 13:21:11 +0530

    backend almost done and api testing success

--- backend/route/Exercise.Route.js
+++ backend/route/Exercise.Route.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const Exercise = require("../models/Exercise.Model");
 let ExerciseModel = require("../models/Exercise.Model");
 
 router.route("/").get((req, res) => {
@@ -27,6 +26,36 @@ router.route("/add").post((req, res) => {
     .catch((err) => {
       res.status(400).json("Error:" + err);
     });
+});
+
+router.route("/:id").get((req, res) => {
+  let id = req.params.id;
+  ExerciseModel.findById(id)
+    .then((exercise) => res.json(exercise))
+    .catch((err) => res.status(400).json("Error:" + err));
+});
+
+router.route("/delete/:id").get((req, res) => {
+  let id = req.params.id;
+  ExerciseModel.findByIdAndDelete(id)
+    .then(() => res.json("Exercise Deleted"))
+    .catch((err) => res.status(400).json("Error:" + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  let id = req.params.id;
+  ExerciseModel.findByIdAndUpdate(id)
+    .then((exercise) => {
+      exercise.username = req.body.username;
+      exercise.duration = Number(req.body.duration);
+      exercise.description = req.body.description;
+      exercise.date = Date.parse(req.body.date);
+      exercise
+        .save()
+        .then(() => res.json("Updated Successfully!"))
+        .catch((err) => res.status(400).json("Error:" + err));
+    })
+    .catch((err) => res.status(400).json("Error:" + err));
 });
 
 module.exports = router;
